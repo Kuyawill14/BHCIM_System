177910def0b181d8f27eec51929d52cf7c8fb94e
Wilson Magaoay <wilson.wiflab@gmail.com>
2023-01-29 00:35:17 +0800

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ import router from "./router/router";
 import VueSweetalert2 from 'vue-sweetalert2';
 import moment from 'moment';
 Vue.prototype.moment = moment;
-// If you don't need the styles, do not connect
+
 import 'sweetalert2/dist/sweetalert2.min.css';
 Vue.use(VueSweetalert2);
 import globalMixin from './mixins/globalMixins'
--- resources/js/components/pages/check-up/check-up-record.vue
+++ resources/js/components/pages/check-up/check-up-record.vue
@@ -66,6 +66,7 @@ export default {
         checkDetails,
         viewRecord,
         updateRecord
+        
     },
     data() {
         return {
